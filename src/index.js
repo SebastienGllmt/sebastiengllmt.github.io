eb3003c3cbafd15ea7157aa28e4428478d10a0d7
sebastiengllmt <sebastiengllmt@gmail.com>
2018-10-17 23:16:15 +0900

    first version

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import { SocialIcon } from 'react-social-icons';
+
+function Introduction() {
+    return(
+    <div>
+        <h1><img src="public/me.jpg" alt="" width='75px' height='75px'/> Introduction </h1>
+        <p>My name is Sebastien Guillemot. </p>
+        <p>I work as an R&amp;D Engineering Lead for <a href="https://emurgo.io/">Emurgo</a> focusing on the <a href="https://www.cardano.org">Cardano</a> project. I write code and create educational content.</p>
+        <h3> How to pronounce my name</h3>
+        <audio src="public/audio/sebastien.mp3" controls/>< br/>
+        <audio src="public/audio/guillemot.mp3" controls/>
+        <h2>Personal channels</h2>
+        <SocialIcon url="http://twitter.com/SebastienGllmt" />
+        <SocialIcon url="https://github.com/SebastienGllmt" />
+        <SocialIcon url="https://www.youtube.com/c/SebastienGuillemot" />
+        <h2>Work channels</h2>
+        <SocialIcon url="https://www.youtube.com/channel/UCgFQ0hHuPO1QDcyP6t9KZTQ" />
+        <SocialIcon url="https://medium.com/@emurgo_io" />
+    </div>
+    );
+}
+
+function Notes() {
+    return(
+    <div>
+        <h1>Notes</h1>
+        <p>Check out some notes I took (embedded within the papers)</p>
+        <ul>
+            <li><a href="public/papers/nipopows_annotated.pdf">NiPoPoW</a></li>
+        </ul>
+    </div>
+    );
+}
+
+ReactDOM.render(<div className='markdown-body'><Introduction /><Notes /></div>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
