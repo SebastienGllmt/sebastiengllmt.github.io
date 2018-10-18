b096b4d0a885f936b919c007313691a65208b168
sebastiengllmt <sebastiengllmt@gmail.com>
2018-10-18 22:46:41 +0900

    add missing env var uses

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { SocialIcon } from 'react-social-icons';
 function Introduction() {
     return(
     <div>
-        <h1><img src="public/me.jpg" alt="" width='75px' height='75px'/> Introduction </h1>
+        <h1><img src={process.env.PUBLIC_URL + "/me.jpg"} alt="" width='75px' height='75px'/> Introduction </h1>
         <p>My name is Sebastien Guillemot. </p>
-        <p>I work as an R&amp;D Engineering Lead for <a href="https://emurgo.io/"><img src="public/emurgo.png" alt="" width='15px' height='15px'/>Emurgo</a> focusing on the <a href="https://www.cardano.org"><img src="public/cardano.png" alt="" width='15px' height='15px'/>Cardano</a> project. I write code and create educational content.</p>
+        <p>I work as an R&amp;D Engineering Lead for <a href="https://emurgo.io/"><img src={process.env.PUBLIC_URL + "/emurgo.png"} alt="" width='15px' height='15px'/>Emurgo</a> focusing on the <a href="https://www.cardano.org"><img src={process.env.PUBLIC_URL + "/cardano.png"} alt="" width='15px' height='15px'/>Cardano</a> project. I write code and create educational content.</p>
         <h3> How to pronounce my name</h3>
         <audio src={process.env.PUBLIC_URL + "/audio/sebastien.mp3"} controls/>< br/>
         <audio src={process.env.PUBLIC_URL + "/audio/guillemot.mp3"} controls/>
