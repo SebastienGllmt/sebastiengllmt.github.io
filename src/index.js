5ecf636d62fda2ac7f66b957fcf4dbc3a1dda331
Sebastien Guillemot <sebastiengllmt@gmail.com>
2021-11-30 21:56:14 +0900

    Update to dcSpark

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ function Introduction() {
     <div>
         <h1><img src={process.env.PUBLIC_URL + "/me.jpg"} alt="" width='75px' height='75px'/> Introduction </h1>
         <p>My name is Sebastien Guillemot. </p>
-        <p>I work as an R&amp;D Engineering Lead for <a href="https://emurgo.io/"><img src={process.env.PUBLIC_URL + "/emurgo.png"} alt="" width='15px' height='15px'/>Emurgo</a> focusing on the <a href="https://www.cardano.org"><img src={process.env.PUBLIC_URL + "/cardano.png"} alt="" width='15px' height='15px'/>Cardano</a> project. I write code and create educational content.</p>
+        <p>I'm the CTO &amp; co-founder of <a href="https://dcspark.io/"><img src={process.env.PUBLIC_URL + "/dcSpark.png"} alt="" width='15px' height='15px'/>dcSpark</a>< br/>
+        I previously worked as the Co-VP of Eng. & Cardano PM for <a href="https://emurgo.io/"><img src={process.env.PUBLIC_URL + "/emurgo.png"} alt="" width='15px' height='15px'/>Emurgo</a><br />
+        I usually focusing on the <a href="https://www.cardano.org"><img src={process.env.PUBLIC_URL + "/cardano.png"} alt="" width='15px' height='15px'/>Cardano</a> project where I write code, standards and create educational content.</p>
         <h3> How to pronounce my name</h3>
         <audio src={process.env.PUBLIC_URL + "/audio/sebastien.mp3"} controls/>< br/>
         <audio src={process.env.PUBLIC_URL + "/audio/guillemot.mp3"} controls/>
@@ -19,8 +21,9 @@ function Introduction() {
         <SocialIcon url="https://github.com/SebastienGllmt" />
         <SocialIcon url="https://www.youtube.com/c/SebastienGuillemot" />
         <h2>Work channels</h2>
-        <SocialIcon url="https://www.youtube.com/channel/UCgFQ0hHuPO1QDcyP6t9KZTQ" />
-        <SocialIcon url="https://medium.com/@emurgo_io" />
+        <SocialIcon url="https://www.youtube.com/channel/UC-S2_W1qS9YcKxhx6vt0SjQ" />
+        <SocialIcon url="https://medium.com/dcspark" />
+        <SocialIcon url="http://twitter.com/dcspark_io" />
     </div>
     );
 }
